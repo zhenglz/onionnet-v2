a30178d817d3c51b07cfc7eccdd8c64edd70f87e
zlzzheng <zlzzheng@tencent.com>
2020-02-08 20:42:20 +0800

    update install.sh and argparser in featurization module

--- generate_features.py
+++ generate_features.py
@@ -5,11 +5,110 @@ import pandas as pd
 import mdtraj as mt
 import itertools
 import multiprocessing
-import sys
-import os
-
+import sys, os
 from biopandas.mol2 import PandasMol2
+import argparse
+from argparse import RawDescriptionHelpFormatter
+from rdkit import Chem
+import subprocess as sp
 
+
+# Define required atomic element type
+elements_ligand = ["H", "C", "CAR", "O", "N",
+                   "S", "P", "DU", "Br", "Cl", "F"]
+elements_protein = ["H", "C", "O", "N", "S", "DU"]
+
+
+class Molecule(object):
+    """Small molecule parser object with Rdkit package.
+
+    Parameters
+    ----------
+    in_format : str, default = 'smile'
+        Input information (file) format.
+        Options: smile, pdb, sdf, mol2, mol
+
+    Attributes
+    ----------
+    molecule_ : rdkit.Chem.Molecule object
+    mol_file : str
+        The input file name or Smile string
+    converter_ : dict, dict of rdkit.Chem.MolFrom** methods
+        The file loading method dictionary. The keys are:
+        pdb, sdf, mol2, mol, smile
+
+
+    """
+
+    def __init__(self, in_format="smile"):
+
+        self.format = in_format
+        self.molecule_ = None
+        self.mol_file = None
+        self.converter_ = None
+        self.mol_converter()
+
+    def mol_converter(self):
+        """The converter methods are stored in a dictionary.
+
+        Returns
+        -------
+        self : return an instance of itself
+
+        """
+        self.converter_ = {
+            "pdb": Chem.MolFromPDBFile,
+            "mol2": Chem.MolFromMol2File,
+            "mol": Chem.MolFromMolFile,
+            "smile": Chem.MolFromSmiles,
+            "sdf": Chem.MolFromMolBlock,
+            "pdbqt": self.babel_converter,
+        }
+
+        return self
+
+    def babel_converter(self, mol_file, output):
+        if os.path.exists(mol_file):
+            try:
+                cmd = 'obabel %s -O %s > /dev/null' % (mol_file, output)
+                job = sp.Popen(cmd, shell=True)
+                job.communicate()
+
+                self.molecule_ = self.converter_['pdb']()
+                return self.molecule_
+            except:
+                return None
+
+    def load_molecule(self, mol_file):
+        """Load a molecule to have a rdkit.Chem.Molecule object
+
+        Parameters
+        ----------
+        mol_file : str
+            The input file name or SMILE string
+
+        Returns
+        -------
+        molecule : rdkit.Chem.Molecule object
+            The molecule object
+
+        """
+
+        self.mol_file = mol_file
+        if not os.path.exists(self.mol_file):
+            print("Molecule file not exists. ")
+            return None
+
+        if self.format not in ["mol2", "mol", "pdb", "sdf", "pdbqt"]:
+            print("File format is not correct. ")
+            return None
+        else:
+            try:
+                self.molecule_ = self.converter_[self.format](self.mol_file)
+            except RuntimeError:
+                return None
+
+            return self.molecule_
 
 class ProteinParser(object):
     """Featurization of Protein-Ligand Complex based on
@@ -18,9 +117,7 @@ class ProteinParser(object):
     Parameters
     ----------
     pdb_fn : str
-        The input pdb file name.
-    lig_code : str
-        The ligand residue name in the input pdb file.
+        The input pdb file name. The file must be in PDB format.
 
     Attributes
     ----------
@@ -35,31 +132,61 @@ class ProteinParser(object):
     distance_computed : bool
         Whether the distances between atoms in receptor and ligand has been computed.
 
+    Examples
+    --------
+    >>> pdb = ProteinParser("input.pdb")
+    >>> pdb.parsePDB('protein and chainid 0')
+    >>> pdb.coordinates_
+    >>> print(pdb.rec_ele)
+
     """
 
     def __init__(self, pdb_fn):
-        self.pdb = mt.load(pdb_fn)
+        self.pdb = mt.load_pdb(pdb_fn)
 
         self.receptor_indices = np.array([])
         self.rec_ele = np.array([])
 
         self.pdb_parsed_ = False
-        self.coordinates = None
+        self.coordinates_ = None
 
     def get_coordinates(self):
-        self.coordinates = self.pdb.xyz[0][self.receptor_indices]
+        """
+        Get the coordinates in the pdb file given the receptor indices.
+
+        Returns
+        -------
+        self : an instance of itself
+
+        """
+        self.coordinates_ = self.pdb.xyz[0][self.receptor_indices]
 
         return self
 
     def parsePDB(self, rec_sele="protein"):
+        """
+        Parse the pdb file and get the detail information of the protein.
+
+        Parameters
+        ----------
+        rec_sele : str,
+            The string for protein selection. Please refer to the following link.
+
+        References
+        ----------
+        Mdtraj atom selection language: http://mdtraj.org/development/atom_selection.html
+
+        Returns
+        -------
+
+        """
         top = self.pdb.topology
-
+        # obtain the atom indices of the protein
         self.receptor_indices = top.select(rec_sele)
-
-        table, bond = top.to_dataframe()
+        _table, _bond = top.to_dataframe()
 
         # fetch the element type of each one of the protein atom
-        self.rec_ele = table['element'][self.receptor_indices].values
+        self.rec_ele = _table['element'][self.receptor_indices].values
         # fetch the coordinates of each one of the protein atom
         self.get_coordinates()
 
@@ -69,10 +196,12 @@ class ProteinParser(object):
 
 
 class LigandParser(object):
-    """
+    """Parse the ligand with biopanda to obtain coordinates and elements.
 
     Parameters
     ----------
+    ligand_fn : str,
+        The input ligand file name.
 
     Methods
     -------
@@ -87,52 +216,66 @@ class LigandParser(object):
     """
 
     def __init__(self, ligand_fn):
-        self.lig = PandasMol2().read_mol2(ligand_fn)
-        #   print(self.lig.df.head())
-        self.lig_data = self.lig.df
+        self.lig_file = ligand_fn
+        self.lig = None
+        self.lig_data = None
 
         self.lig_ele = None
-        self.coordinates = None
+        self.coordinates_ = None
         self.mol2_parsed_ = False
+
+    def _format_convert(self, input, output):
+        mol = Molecule(in_format=input.split(".")[-1])
+        mol.babel_converter(input, output)
+        return self
 
     def get_element(self):
         ele = list(self.lig_data["atom_type"].values)
-        #   print(ele)
-
         self.lig_ele = list(map(get_ligand_elementtype, ele))
-
-        #    print(self.lig_ele)
         return self
 
     def get_coordinates(self):
-        self.coordinates = self.lig_data[['x', 'y', 'z']].values
-        #        print(self.coordinates)
+        """
+        Get the coordinates in the pdb file given the ligand indices.
+
+        Returns
+        -------
+        self : an instance of itself
+
+        """
+        self.coordinates_ = self.lig_data[['x', 'y', 'z']].values
         return self
 
     def parseMol2(self):
         if not self.mol2_parsed_:
+            if self.lig_file.split(".")[-1] != "mol2":
+                out_file = self.lig_file + ".mol2"
+                self._format_convert(self.lig_file, out_file)
+                self.lig_file = out_file
+
+            if os.path.exists(self.lig_file):
+                self.lig = PandasMol2().read_mol2(self.lig_file)
+            else:
+                return None
+            self.lig_data = self.lig.df
             self.get_element()
             self.get_coordinates()
-
             self.mol2_parsed_ = True
 
         return self
 
 
 def get_protein_elementtype(e):
-    all_elements = ["H", "C", "O", "N", "S", "DU"]
-    if e in all_elements:
+    if e in elements_protein:
         return e
     else:
         return "DU"
 
 
 def get_ligand_elementtype(e):
-    all_elements = ["H", "C", "CAR", "Br", "Cl", "P", "F", "O", "N", "S", "DU"]
-    # print(e, e.split(".")[0])
     if e == "C.ar":
         return "CAR"
-    elif e.split(".")[0] in all_elements:
+    elif e.split(".")[0] in elements_ligand:
         return e.split(".")[0]
     else:
         return "DU"
@@ -144,7 +287,6 @@ def atomic_distance(dat):
 
 def distance_pairs(coord_pro, coord_lig):
     pairs = list(itertools.product(coord_pro, coord_lig))
-    #   print(pairs[:10])
     distances = map(atomic_distance, pairs)
 
     return list(distances)
@@ -159,6 +301,7 @@ def distance2counts(megadata):
 
 def generate_features(args):
     pro_fn, lig_fn, n_cutoffs = args
+    print("INFO: Processing %s and %s ..." % (pro_fn, lig_fn))
 
     pro = ProteinParser(pro_fn)
     pro.parsePDB()
@@ -167,18 +310,15 @@ def generate_features(args):
     # print(pro.rec_ele)
     for i, d in enumerate(['x', 'y', 'z']):
         # coordinates by mdtraj in unit nanometer
-        protein_data[d] = pro.coordinates[:, i] * 10.0
+        protein_data[d] = pro.coordinates_[:, i]
 
     lig = LigandParser(lig_fn)
     lig.parseMol2()
     ligand_data = pd.DataFrame()
     ligand_data['element'] = lig.lig_ele
     for i, d in enumerate(['x', 'y', 'z']):
-        ligand_data[d] = lig.coordinates[:, i]
-
-    # print("LIGAND COORD GENERATE")
-    elements_ligand = ["H", "C", "CAR", "O", "N", "S", "P", "DU", "Br", "Cl", "F"]
-    elements_protein = ["H", "C", "O", "N", "S", "DU"]
+        # the coordinates in ligand are in angstrom
+        ligand_data[d] = lig.coordinates_[:, i] * 0.1
 
     onionnet_counts = pd.DataFrame()
 
@@ -187,68 +327,110 @@ def generate_features(args):
             protein_xyz = protein_data[protein_data['element'] == ep][['x', 'y', 'z']].values
             ligand_xyz = ligand_data[ligand_data['element'] == el][['x', 'y', 'z']].values
 
-            #       print(ligand_xyz[:10], protein_xyz[:10])
-            # distances = distance_pairs(protein_xyz, ligand_xyz)
-            # print(distances[:10])
             counts = np.zeros(len(n_cutoffs))
 
-            #       print(el, ep, "GET ELE TYPE SPEC DISTANCES")
-            if len(protein_xyz) and len(ligand_xyz):
-                # print(protein_xyz.shape, ligand_xyz.shape)
+            if protein_xyz.shape[0] and ligand_xyz.shape[0]:
                 distances = distance_pairs(protein_xyz, ligand_xyz)
 
-                # print(sorted(distances)[:10], sorted(distances)[-10:])
                 for i, c in enumerate(n_cutoffs):
                     single_count = distance2counts((distances, c))
                     if i > 0:
-                        single_count = single_count - counts[i - 1]
+                        single_count = single_count - np.sum(counts[i - 1])
                     counts[i] = single_count
 
             feature_id = "%s_%s" % (el, ep)
             onionnet_counts[feature_id] = counts
 
-    return list(onionnet_counts.values.ravel()), pro_fn
+    return list(onionnet_counts.values.ravel()), pro_fn+"_"+lig_fn
 
 
 def main():
-    inp = sys.argv[1]
-    out = sys.argv[2]
+    d = """
+       Predicting protein-ligand binding affinities (pKa) with OnionNet model.
+       
+       Citation: Zheng L, Fan J, Mu Y. arXiv preprint arXiv:1906.02418, 2019.
+       Author: Liangzhen Zheng (zhenglz@outlook.com)
+       This script is used to generate inter-molecular element-type specific
+       contact features. Installation instructions should be refered to
+       https://github.com/zhenglz/onionnet-v2
+       
+       Examples:
+       Show help information
+       python generate_features.py -h
+       Run the script
+       python generate_features.py -inp input_samples.dat -out features_samples.csv
+       # tutorial example
+       cd tuttorials/PDB_samples
+       python ../../generate_features.py -inp input_PDB_testing.dat -out 
+       PDB_testing_features.csv
+    """
+    parser = argparse.ArgumentParser(description=d,
+                                     formatter_class=RawDescriptionHelpFormatter)
+    parser.add_argument("-inp", type=str, default="input.dat",
+                        help="Input. The input file containg the file path of each \n"
+                             "of the protein-ligand complexes files (in pdb format.)\n"
+                             "There should be only 1 column, each row or line containing\n"
+                             "the input file path, relative or absolute path.")
+    parser.add_argument("-out", type=str, default="output.csv",
+                        help="Output. Default is output.csv \n"
+                             "The output file name containing the features, each sample\n"
+                             "per row. ")
+    parser.add_argument("-nt", type=int, default=1,
+                        help="Input, optional. Default is 1. "
+                             "Use how many of cpu cores.")
+    parser.add_argument("-upbound", type=float, default=3.1,
+                        help="Input, optional. Default is 3.1 nm. "
+                             "The largest distance cutoff.")
+    parser.add_argument("-lowbound", type=float, default=0.1,
+                        help="Input, optional. Default is 0.1 nm. "
+                             "The lowest distance cutoff.")
+    parser.add_argument("-nbins", type=int, default=61,
+                        help="Input, optional. Default is 61. "
+                             "The number of distance cutoffs.")
 
-    data_root = './general_set'
+    args = parser.parse_args()
 
-    num_threads = 1
+    if len(sys.argv) < 2:
+        parser.print_help()
+        sys.exit(0)
 
-    n_cutoffs = np.linspace(0.1, 3.1, 61)
+    num_threads = args.nt
 
-    with open(inp)  as lines:
-        codes = [x for x in lines if ("#" not in x and len(x.split()))]
+    n_cutoffs = np.linspace(args.lowbound,
+                            args.upbound, args.nbins)
+
+    with open(args.inp) as lines:
+        codes = [x.split() for x in lines if
+                 ("#" not in x and len(x.split()))]
 
     input_arguments = []
     for c in codes:
-        r_fn = os.path.join(data_root, "%s/%s_protein.pdb" % (c, c))
-        l_fn = os.path.join(data_root, "%s/%s_ligand.mol2" % (c, c))
-
+        r_fn, l_fn = c[0], c[1]
         input_arguments.append((r_fn, l_fn, n_cutoffs))
 
     if num_threads <= 1:
         dat = []
         labels = []
         for item in input_arguments:
-            r = generate_features(item)
-            dat.append(r[0])
-            labels.append(r[1])
+            ocontacts, fn = generate_features(item)
+            dat.append(ocontacts)
+            labels.append(fn)
     else:
         pool = multiprocessing.Pool(num_threads)
         results = pool.map(generate_features, input_arguments)
 
         pool.close()
         pool.join()
-
         dat = [x[0] for x in results]
         labels = [x[1] for x in results]
 
-    features = pd.DataFrame(dat, index=labels)
+    elecombin =list(itertools.product(elements_ligand, elements_protein)) \
+               * n_cutoffs.shape[0]
+    elecombin = ["_".join(x) for x in elecombin]
+    features = [elecombin[i]+"_"+str(i+1) for i in
+                range(len(elecombin) * n_cutoffs.shape[0])]
 
-    features.to_csv(out, header=False, index=True)
+    features = pd.DataFrame(dat, index=labels, columns=features)
+    features.to_csv(args.out, header=True, index=True)
 
-    print("Completed.")
+    print("INFO: Feature extraction completed.")
--- install.sh
+++ install.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-# Install necessary python modules with conda
+#Check and install conda
 conda_exe=$(which conda)
 length_code=${#conda_exe}
 if [ $length_code -lt 0 ]; then
-  conda create -n onionnet python=3.6
+  echo "Anaconda or Miniconda found in: $conda_exe"
+else
+  echo "No anaconda or miniconda, installing miniconda3 now ..."
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME/miniconda.sh
+  bash $HOME/miniconda.sh -b -p $HOME/miniconda3
+  echo ""
+  # add path to bashrc
+  echo "Add Miniconda3 to bashrc PATH variable. "
+  echo 'export PATH="$PATH:$HOME/miniconda3/bin"' >> $HOME/.bashrc
+  rm $HOME/miniconda.sh
+
+  # get current dir
+  current_dir=$(echo $PWD)
+  source $HOME/.bashrc
+  cd $current_dir
 fi
 
+#Create a python environment
+conda create -n onionnet python=3.6
 conda activate onionnet
-
+#Install necessary python modules with conda
 conda install -c rdkit rdkit
 conda install -c omnia mdtraj
 conda install -c openbabel openbabel
+conda install biopandas -c conda-forge
 conda install numpy pandas scipy
 conda install tensorflow
 
+#Finish installation
+echo "Installation completed. To use the package, please enable the environment by: "
+echo "conda activate onionnet"
